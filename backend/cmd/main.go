361648140951aa5abaec3e1b7878d20c73497de2
Akshat Gupta <akshat.g1707@gmail.com>
2025-02-07 20:47:18 +0530

    feat: logger

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,13 +4,26 @@ import (
 	"net/http"
 
 	routes "github.com/Oik17/yantra-hack/internal/routers"
+	"github.com/Oik17/yantra-hack/internal/utils"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	utils.PrintSiamBanner()
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, Echo!"})
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_custom}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"}` + "\n",
+		CustomTimeFormat: "02/01/2006 15:04:05",
+	}))
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "pong"})
 	})
 
 	routes.WeatherRoutes(e)
