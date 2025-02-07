0f0d247236c0f88b674d549751db705495679b45
Ryan <ryanj1504@gmail.com>
2025-02-08 01:53:46 +0530

    feat: home screen

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final Color primaryOrange = Color(0xFFFF9800);
+final Color lightOrange = Color(0xFFFFE0B2);
+final Color darkOrange = Color(0xFFF57C00);
+final Color bgColor = Colors.white;
+final Color textDark = Color(0xFF424242);
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:yantra_blackspace/screens/data_screen.dart';
+import 'package:yantra_blackspace/screens/home_screen.dart';
+import 'package:yantra_blackspace/screens/profile_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart City Solar App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => HomeScreen(),
+        '/data' : (context) => DataScreen(),
+        '/profile' : (context) => ProfileScreen(),
+      },
+    );
+  }
+}
